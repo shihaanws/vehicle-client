e21040842ab55101d28bcf96df0ea7a197f83a2b
shihaan <shihaanws@gmail.com>
2022-03-16 17:35:12 +0530

    client files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+const axios = require("axios");
 
 function App() {
+  const [users, setUsers] = useState([
+    { id: 6, password: "", username: "gokul" },
+  ]);
+
+  const [username1, setUsername1] = useState("testname");
+
+  const getAllUsers = () => {
+    axios
+      .get("http://localhost:1234/getAllUsers")
+      .then((response) => {
+        setUsers(response.data);
+        console.log(response.data);
+        console.log(username1.username);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const createUser = () => {
+    axios
+      .post("http://localhost:1234" + "/login", {
+        username: username1.username,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  const deleteUser = (idey) => {
+    console.log(idey);
+    axios.post("http://localhost:1234" + `/delete/${idey}`).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        onChange={(e) => {
+          setUsername1({ username: e.target.value });
+        }}
+      />
+      <button onClick={createUser}>Add user</button>
+
+      {users.map((ele, index) => {
+        return (
+          <p id={index}>
+            {ele.username} --- {index}
+            <button
+              id={index + 1}
+              onClick={(e) => {
+                deleteUser(e.target.id);
+              }}
+            >
+              Delete me
+            </button>
+          </p>
+        );
+      })}
+      <button onClick={getAllUsers}>View all users</button>
     </div>
   );
 }
-
 export default App;
